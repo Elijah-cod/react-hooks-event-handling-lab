e485341b8151bf2dddeb308b5ad5370eaff7387a
Elijah <iamngunjiri@gmail.com>
2025-01-20 13:07:16 +0300

    Complete Event Handling in React Lab

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,11 @@
 // Code Keypad Component Here
 
 function Keypad (){
+    function handleOnChange(){
+        console.log('Entering password...')
+    }
     return (
-        <div></div>
+        <input type="password" onChange={handleOnChange}/>
     )
 }
 
